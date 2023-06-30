a19ab4afb81f5158513b1fb2dec5b2ed6b867e93
Michael Peter Christen <mc@yacy.net>
2023-07-01 00:11:50 +0200

    added a large context injection as alternative and/or additon to RAG

--- expert_server.py
+++ expert_server.py
@@ -16,9 +16,13 @@ CORS(app)
 YACYSEARCH_HOST = "http://localhost:8094"
 OPENAI_API_HOST = "https://api.openai.com"
 OPENAI_API_KEY  = ""
+RAG = False
 RAG_CONTEXT_PREFIX = "If necessary, use the following context to answer the question. If this text does not match the question, ignore it."
 #RAG_CONTEXT_PREFIX = "Wenn nötig, verwende den folgenden Kontext, um die Frage zu beantworten. Wenn dieser Text nicht zur Frage passt, ignorieren Sie ihn."
-
+SYSTEM = False
+SYSTEM_CONTEXT_PREFIX = "If necessary, use the following context to answer the question. If this text does not match the question, ignore it."
+SYSTEM_CONTEXT_PATH = "knowledge"
+    
 # overload settings with environment variables in case they are set
 if 'YACYSEARCH_HOST' in os.environ: YACYSEARCH_HOST = os.environ['YACYSEARCH_HOST']
 if 'OPENAI_API_HOST' in os.environ: OPENAI_API_HOST = os.environ['OPENAI_API_HOST']
@@ -34,55 +38,72 @@ def proxy():
         response = make_response()
         return response
     elif request.method in ['GET', 'POST']:
+
         # read the content from the users request that was sent out with chat.html
         body = request.get_json()
         messages = body['messages']
-        # get the content object from the latest message: this is the current prompt
-        prompt = messages[-1]['content']
+            
+        if SYSTEM and os.path.isdir(SYSTEM_CONTEXT_PATH):
+            # read the content from the users request that was sent out with chat.html
+            systemmessage = messages[0]['content']
+            # read the large context
+            largetext = ""
+            for filename in os.listdir(SYSTEM_CONTEXT_PATH):
+                if filename.endswith(".txt") or filename.endswith(".md"):
+                    with open(os.path.join(SYSTEM_CONTEXT_PATH, filename), 'r') as f: largetext += f.read() + '\n\n'
+            # patch the system message
+            messages[0]['content'] = systemmessage + '\n\n' + SYSTEM_CONTEXT_PREFIX + '\n\n' + largetext
+            print(systemmessage)
+            # todo: set n_keep
 
-        # search for a RAG document that matches the prompt
-        try:
-            searchresult = requests.post(YACYSEARCH_HOST + '/yacysearch.json', json={'query': prompt, 'count': 6})
-            context = ""
-            if searchresult.status_code == 200:
-                try:
-                    # parse the result to json
-                    searchresult_json = json.loads(searchresult.text)
-                    items = searchresult_json['channels'][0]['items']
-                    print(items)
-                    # number of results
-                    hitcount = len(items)
-                    minimum_distance = 70
+        if RAG:
+            # get the content object from the latest message: this is the current prompt
+            prompt = messages[-1]['content']
 
-                    for i in range(0, min(hitcount, 5)):
-                        if items[i]['distance'] < minimum_distance - (i * 10):
-                            description = items[i]['description']
-                            title = items[0]['title']
-                            title = title[0] if isinstance(title, list) and len(title) > 0 else str(title)
-                            if len(description) > 600: description = description[:600] + "..."
-                            context = context + '\n\n' + description
-                            contextlog = contextlog + "&diams; added RAG document: " + title + "\n"
-                
-                    print(context)
-                    # replace the content object with the enriched content in the body object
-                except json.decoder.JSONDecodeError as e:
-                    # Handle invalid JSON response from search API
-                    print(f"Error: Search API returned invalid JSON: {e}")
+            # search for a RAG document that matches the prompt
+            try:
+                searchresult = requests.post(YACYSEARCH_HOST + '/yacysearch.json', json={'query': prompt, 'count': 6})
+                context = ""
+                if searchresult.status_code == 200:
+                    try:
+                        # parse the result to json
+                        searchresult_json = json.loads(searchresult.text)
+                        items = searchresult_json['channels'][0]['items']
+                        print(items)
+                        # number of results
+                        hitcount = len(items)
+                        minimum_distance = 70
 
-                # add the new context to the prompt if the context length is > 0
-                if len(context) > 0:
-                    prompt = prompt + '\n\n' + RAG_CONTEXT_PREFIX + context
+                        for i in range(0, min(hitcount, 5)):
+                            if items[i]['distance'] < minimum_distance - (i * 10):
+                                description = items[i]['description']
+                                title = items[0]['title']
+                                title = title[0] if isinstance(title, list) and len(title) > 0 else str(title)
+                                if len(description) > 600: description = description[:600] + "..."
+                                context = context + '\n\n' + description
+                                contextlog = contextlog + "&diams; added RAG document: " + title + "\n"
+                    
+                        print(context)
+                        # replace the content object with the enriched content in the body object
+                    except json.decoder.JSONDecodeError as e:
+                        # Handle invalid JSON response from search API
+                        print(f"Error: Search API returned invalid JSON: {e}")
 
-            # write back the enriched prompt to the body object
-            messages[-1]['content'] = prompt
-        except requests.exceptions.ConnectionError as e:
-            # Handle connection error to search API
-            print(f"Error: Search API connection error: {e}")
+                    # add the new context to the prompt if the context length is > 0
+                    if len(context) > 0:
+                        prompt = prompt + '\n\n' + RAG_CONTEXT_PREFIX + context
 
-        # get the body object back into a string
-        body['model'] = "gpt-3.5-turbo"
-        #body['type'] = "json_object"
-        body['stream'] = True # this return format is stream only
+                # write back the enriched prompt to the body object
+                messages[-1]['content'] = prompt
+            except requests.exceptions.ConnectionError as e:
+                # Handle connection error to search API
+                print(f"Error: Search API connection error: {e}")
+
+            # get the body object back into a string
+            body['model'] = "gpt-3.5-turbo"
+            #body['type'] = "json_object"
+            body['stream'] = True # this return format is stream only
+        
         encoded_body = json.dumps(body).encode('utf-8') 
         print(encoded_body)
 
@@ -99,7 +120,7 @@ def proxy():
         headers['Content-Length'] = str(len(encoded_body)) # content length must be re-computed because it might have changed
         if OPENAI_API_KEY != "": headers['Authorization'] = 'Bearer ' + OPENAI_API_KEY
         conn.request("POST", api_url.path, encoded_body, headers)
-        print("sent request to OpenAI API")
+        #print("sent request to OpenAI API")
         
         # Thread function for reading from the server
         def read_from_upstream():
@@ -108,7 +129,7 @@ def proxy():
                 line = resp.readline()
                 if line:
                     t = line.decode('utf-8')
-                    print(t)
+                    #print(t)
                     yield line
                     if t.find("data: [DONE]") != -1: break
                 else:
@@ -128,10 +149,18 @@ if __name__ == "__main__":
     parser.add_argument("--yacy_search_host", default=YACYSEARCH_HOST, type=str, help="http address of search host")
     parser.add_argument("--openai_api_host", default=OPENAI_API_HOST, type=str, help="http address of ApenAI API")
     parser.add_argument("--openai_api_key", default=OPENAI_API_KEY, type=str, help="OpenAI API key")
+    parser.add_argument("--rag", default=False, action='store_true', help="Enable RAG (Retrieval Augmented Generation) feature")
     parser.add_argument("--rag_context_prefix", default=RAG_CONTEXT_PREFIX, type=str, help="Prefix string for RAG context")
+    parser.add_argument("--system", default=False, action='store_true', help="Enable large system context prompts")
+    parser.add_argument("--system_context_prefix", default=SYSTEM_CONTEXT_PREFIX, type=str, help="Prefix string for large system context")
+    parser.add_argument("--system_context_path", default=SYSTEM_CONTEXT_PATH, type=str, help="Path to files to be put into system context")
     args = parser.parse_args()
     YACYSEARCH_HOST = args.yacy_search_host
     OPENAI_API_HOST = args.openai_api_host
     OPENAI_API_KEY = args.openai_api_key
+    RAG = args.rag
     RAG_CONTEXT_PREFIX = args.rag_context_prefix
+    SYSTEM = args.system
+    SYSTEM_CONTEXT_PREFIX = args.system_context_prefix
+    SYSTEM_CONTEXT_PATH = args.system_context_path
     app.run(host=args.host, port=args.port, debug=True)
